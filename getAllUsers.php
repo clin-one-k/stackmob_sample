46c858f8f7c66f27ba90c7d0d99945be5c773ba4
Chen Lin <clin@one-k.com>
2013-09-26 14:57:34 -0700

    modify getAllUsrs.php

--- getAllUsers.php
+++ getAllUsers.php
@@ -1,39 +1,44 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-include("key.php");
-$key=KEY;
+// for debug
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_errors', 1 );
+error_reporting ( -1 );
 
-$curl = curl_init("http://api.stackmob.com/user");
-//$fp = fopen("example_homepage.txt", "w");
+// key setup
+include( "key.php" );
+$key = KEY ;
 
-curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+// error message
+$error_message = "";
+
+// prepare for command-line URL tool
+$curl = curl_init ( "http://api.stackmob.com/user" );
+curl_setopt ( $curl, CURLOPT_HEADER, true );
+curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+curl_setopt ( $curl, CURLOPT_HTTPHEADER, array (
 	"Accept: application/vnd.stackmob+json; version=0",
-	"X-stackMob-API-Key: " .$key
+	"X-stackMob-API-Key: ".$key
 	)
 );
-$response=curl_exec($curl);
 
-$j=json_decode(curl_exec($curl),true);
+// run crul
+$response = curl_exec ( $curl );
+
+// process return
+//$j=json_decode(curl_exec($curl),true);
 $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        //$this->log->debug("Response: $response");
-        //$this->log->debug("Status code: $statusCode");
-        if (!$response && $statusCode !== 200) {
-             $response = curl_error($curl);
-             curl_close($curl);
+if ( !$response && $statusCode !== 200 ) {
+     $response = curl_error ( $curl );
+     curl_close ( $curl );
+     $error_message="curl failed.";
+} else {
+    list( $header, $body ) = explode ( "\r\n\r\n", $response, 2 );
+    $_response = $body;
+    $_results = null;
+    $decoded = json_decode ( $body );
+}
+curl_close ( $curl );
 
-             throw new StackmobException($response, $statusCode);
-        } else {
-
-            list($header, $body) = explode("\r\n\r\n", $response, 2);
-            $_response = $body;
-            $_results = null;
-            $decoded = json_decode($body);
-        }
-curl_close($curl);
 ?>
 <h1>Get all usrs</h1>
 <p>Use REST for PHP to retrieve all the user from stackMob. 
@@ -45,3 +50,4 @@ curl_close($curl);
 	<tr><td><?php echo $r->username?></td><td><?php echo $r->profession?></td></tr>
 <?php endforeach;?>
 </table>
+<?php echo $error_message;?>
