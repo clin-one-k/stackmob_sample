79449a77861e02096d4e31c809f02e198c74dad7
Chen Lin <clin@one-k.com>
2013-09-26 15:05:12 -0700

    Add user php file and JS library

---
+++ addUser.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <!--<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>-->
+    <script src="http://static.stackmob.com/js/stackmob-js-0.9.2-bundled-min.js"></script>
+    <script src="key.js"></script>
+    <script src="app.js"></script>
+</head>
+
+<script>
+function addUser(name,password,profession){
+    var name=document.forms["userForm"]["username"].value;
+    var password=document.forms["userForm"]["password"].value;
+    var profession=document.forms["userForm"]["profession"].value;
+
+    //alert(name+" "+password+" "+profession); for debug only
+
+    if(!name || !password || ! profession){
+        alert("field missing!");
+        return;
+    }
+    
+    var user = new StackMob.User({ 
+        username: name, 
+        password: password, 
+        profession: profession  
+    });
+    user.create({
+        success: function( model, result, options) {
+            alert( "add user successed" );
+        },
+        error: function( model, result, options) {
+            alert("add user failed")
+        }
+    });
+}
+</script>
+<body>
+    <h1>Add a new user to stackMob</h1>
+    <p>Use Javascript provided by stackMob. Reference: https://developer.stackmob.com/js-sdk/developer-guide#Users. Check the result on https://dashboard.stackmob.com/data/browser/user?avn=0&oauthvn=One</p>
+    <form name="userForm">
+    <br/>Username: <input type="text" name="username" id="username">
+    <br/>Password: <input type="text" name="password" id="password">
+    <br/>Profession: <input type="text" name="profession" id="profession">
+    <button type="button" onClick="addUser()">Add new user</button>
+    </form>
+</body>
+</html>
