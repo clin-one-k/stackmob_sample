80928d598ae775f7414e58e3d7107772a985ab13
Chen Lin <clin@one-k.com>
2013-09-26 14:59:34 -0700

    modify getAllUsrs.php

--- getAllUsers.php
+++ getAllUsers.php
@@ -16,9 +16,9 @@ $curl = curl_init ( "http://api.stackmob.com/user" );
 curl_setopt ( $curl, CURLOPT_HEADER, true );
 curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
 curl_setopt ( $curl, CURLOPT_HTTPHEADER, array (
-	"Accept: application/vnd.stackmob+json; version=0",
-	"X-stackMob-API-Key: ".$key
-	)
+    "Accept: application/vnd.stackmob+json; version=0",
+    "X-stackMob-API-Key: ".$key
+    )
 );
 
 // run crul
@@ -42,12 +42,12 @@ curl_close ( $curl );
 ?>
 <h1>Get all usrs</h1>
 <p>Use REST for PHP to retrieve all the user from stackMob. 
-	https://developer.stackmob.com/rest-api/api-docs#a-get_-_read_objects and 
-	https://github.com/jobiwankanobi/stackmobphp</p>
+    https://developer.stackmob.com/rest-api/api-docs#a-get_-_read_objects and 
+    https://github.com/jobiwankanobi/stackmobphp</p>
 <table border>
 <tr><th>username</th><th>profession</th></tr>
 <?php foreach ($decoded as $r):?>
-	<tr><td><?php echo $r->username?></td><td><?php echo $r->profession?></td></tr>
+    <tr><td><?php echo $r->username?></td><td><?php echo $r->profession?></td></tr>
 <?php endforeach;?>
 </table>
 <?php echo $error_message;?>
