d964accbbb390eebdc519cdd87628ca63dc49c73
Chen Lin <clin@one-k.com>
2013-09-27 14:29:58 -0700

    add new field

--- addUser.php
+++ addUser.php
@@ -3,19 +3,25 @@
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <!--<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>-->
     <script src="http://static.stackmob.com/js/stackmob-js-0.9.2-bundled-min.js"></script>
-    <script src="key.js"></script>
-    <script src="app.js"></script>
+    <script src="key.js"></script>   
 </head>
-
 <script>
+
 function addUser(name,password,profession){
+    //initial Keys
+    StackMob.init({
+        publicKey: publicKey,
+        apiVersion: 0
+    });
+
     var name=document.forms["userForm"]["username"].value;
     var password=document.forms["userForm"]["password"].value;
     var profession=document.forms["userForm"]["profession"].value;
+    var areaCode=document.forms["userForm"]["areaCode"].value;
 
     //alert(name+" "+password+" "+profession); for debug only
 
-    if(!name || !password || ! profession){
+    if(!name || !password || ! profession || ! areaCode){
         alert("field missing!");
         return;
     }
@@ -23,8 +29,10 @@ function addUser(name,password,profession){
     var user = new StackMob.User({ 
         username: name, 
         password: password, 
-        profession: profession  
+        profession: profession,
+        areaCode: areaCode
     });
+
     user.create({
         success: function( model, result, options) {
             alert( "add user successed" );
@@ -42,6 +50,7 @@ function addUser(name,password,profession){
     <br/>Username: <input type="text" name="username" id="username">
     <br/>Password: <input type="text" name="password" id="password">
     <br/>Profession: <input type="text" name="profession" id="profession">
+    <br/>Area code:<input type="text" name="areaCode" id="areaCode">
     <button type="button" onClick="addUser()">Add new user</button>
     </form>
 </body>
